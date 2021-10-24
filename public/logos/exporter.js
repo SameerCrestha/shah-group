555931cbc6ffc9f5903bd810c5bab23ad74c82cf
SameerCrestha <ddav.crestha@gmail.com>
2021-10-24 20:06:01 +0545

    added exporter for logos

---
+++ public/logos/exporter.js
@@ -0,0 +1,9 @@
+import fb from "./fb.svg";
+import insta from "./insta.svg";
+import location from "./location.svg";
+import mail from "./mail.svg";
+import phone from "./phone.svg";
+import twitter from "./twitter.svg";
+import linkedin from "./linkedin.svg";
+const icons={fb,insta,location,mail,phone,twitter,linkedin};
+export default icons;
