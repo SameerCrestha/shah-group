0848338fa3bef0cce44e3d120415dce67e9f9a2e
divas regmi <divasregmi@gmail.com>
2021-10-25 16:21:45 +0545

    product page finished

--- components/layout.js
+++ components/layout.js
@@ -9,87 +9,182 @@ import phone from '../public/logos/phone.svg';
 import twitter from '../public/logos/twitter.svg';
 import instagram from '../public/logos/insta.svg';
 import facebook from '../public/logos/fb.svg';
-export default function Layout({children,page}){
-    return(
-        <>
-        <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;500;600;700&display=swap" rel="stylesheet"/>
-        </Head>
-        <body>
-            <header className={styles.header}>
-            <h1 className={styles.headerText}>Shah Group</h1>
-            <Link href='/'><a className={page==1?styles.active:""}>Home</a></Link>
-            <Link href='/about-us'><a className={page==2?styles.active:""}>About us</a></Link>
-            <Link href='/products'><a className={page==3?styles.active:""}>Products</a></Link>
-            <Link href='/services'><a className={page==4?styles.active:""}>Services</a></Link>
-            <Link href='/contact-us'><a className={page==5?styles.active:""}>Contact us</a></Link>
+export default function Layout({ children, page }) {
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <header className={styles.header}>
+          <h1 className={styles.headerText}>Shah Group</h1>
+          <Link href="/">
+            <a className={page == 1 ? styles.active : ''}>Home</a>
+          </Link>
+          <Link href="/about-us">
+            <a className={page == 2 ? styles.active : ''}>About us</a>
+          </Link>
+          <Link href="/products">
+            <a className={page == 3 ? styles.active : ''}>Products</a>
+          </Link>
+          <Link href="/services">
+            <a className={page == 4 ? styles.active : ''}>Services</a>
+          </Link>
+          <Link href="/contact-us">
+            <a className={page == 5 ? styles.active : ''}>Contact us</a>
+          </Link>
         </header>
         {children}
         <footer className={styles.footer}>
-        <hr className={styles.footerLine}/>
-        <div className={styles.footerSection1}>
-        <div className={styles.footerInfo}>
-        <h3>Shah Group</h3>
-        <div className={styles.footerInfoItem}><Image src={location} height={20} width={15}/><div className={styles.footerInfoItemText}>Lokanthali,Bhaktapur</div></div>
-        <div className={styles.footerInfoItem}><Image src={phone} height={18} width={15}/><div className={styles.footerInfoItemText}>9856669377</div></div>
-        <div className={styles.footerInfoItem}><Image src={mail} height={13} width={14}/><div className={styles.footerInfoItemText}>Shahgroup@gmail.com</div></div>
-        </div>
-        <div className={styles.footerLinks}>
-            <div>
+          <hr className={styles.footerLine} />
+          <div className={styles.footerSection1}>
+            <div className={styles.footerInfo}>
+              <h3>Shah Group</h3>
+              <div className={styles.footerInfoItem}>
+                <Image src={location} height={20} width={15} />
+                <div className={styles.footerInfoItemText}>
+                  Lokanthali,Bhaktapur
+                </div>
+              </div>
+              <div className={styles.footerInfoItem}>
+                <Image src={phone} height={18} width={15} />
+                <div className={styles.footerInfoItemText}>9856669377</div>
+              </div>
+              <div className={styles.footerInfoItem}>
+                <Image src={mail} height={13} width={14} />
+                <div className={styles.footerInfoItemText}>
+                  Shahgroup@gmail.com
+                </div>
+              </div>
+            </div>
+            <div className={styles.footerLinks}>
+              <div>
                 <h4>Site Map</h4>
                 <p>
-                    <Link href='/'><a>Home</a></Link><br/>
-                    <Link href='/about-us'><a>About us</a></Link><br/>
-                    <Link href='/products'><a >Products</a></Link><br/>
-                    <Link href='/services'><a>Services</a></Link><br/>
-                    <Link href='/contact-us'><a>Contact us</a></Link><br/>
+                  <Link href="/">
+                    <a>Home</a>
+                  </Link>
+                  <br />
+                  <Link href="/about-us">
+                    <a>About us</a>
+                  </Link>
+                  <br />
+                  <Link href="/products">
+                    <a>Products</a>
+                  </Link>
+                  <br />
+                  <Link href="/services">
+                    <a>Services</a>
+                  </Link>
+                  <br />
+                  <Link href="/contact-us">
+                    <a>Contact us</a>
+                  </Link>
+                  <br />
                 </p>
-            </div>
-            <div>
+              </div>
+              <div>
                 <h4>Products</h4>
                 <p>
-                    <Link href='/'><a>Ghee</a></Link><br/>
-                    <Link href='/'><a>Cardamum</a></Link><br/>
-                    <Link href='/'><a >Palm Oil</a></Link><br/>
-                    <Link href='/'><a>Yarn</a></Link><br/>
-                    <Link href='/'><a>Yarsagumba</a></Link><br/>
+                  <Link href="/">
+                    <a>Ghee</a>
+                  </Link>
+                  <br />
+                  <Link href="/">
+                    <a>Cardamum</a>
+                  </Link>
+                  <br />
+                  <Link href="/">
+                    <a>Palm Oil</a>
+                  </Link>
+                  <br />
+                  <Link href="/">
+                    <a>Yarn</a>
+                  </Link>
+                  <br />
+                  <Link href="/">
+                    <a>Yarsagumba</a>
+                  </Link>
+                  <br />
                 </p>
-            </div>
-            <div>
+              </div>
+              <div>
                 <h4>Products</h4>
                 <p>
-                    <Link href='/'><a>Fafar</a></Link><br/>
-                    <Link href='/'><a>Kado</a></Link><br/>
-                    <Link href='/'><a >Tori</a></Link><br/>
-                    <Link href='/'><a>Till</a></Link><br/>
-                    <Link href='/'><a>Acchar</a></Link><br/>
+                  <Link href="/">
+                    <a>Fafar</a>
+                  </Link>
+                  <br />
+                  <Link href="/">
+                    <a>Kado</a>
+                  </Link>
+                  <br />
+                  <Link href="/">
+                    <a>Tori</a>
+                  </Link>
+                  <br />
+                  <Link href="/">
+                    <a>Till</a>
+                  </Link>
+                  <br />
+                  <Link href="/">
+                    <a>Acchar</a>
+                  </Link>
+                  <br />
                 </p>
+              </div>
             </div>
-        </div>
-        </div>
-        <div className={styles.footerSection2}>
+          </div>
+          <div className={styles.footerSection2}>
             <div>Call us: 986475677 | 01-47863936 | 01-567555930</div>
             <div>
-                Follow us on:
-                <a ><Image src={twitter} height={16} width={16}></Image></a>
-                <a><Image src={instagram} height={16} width={16}></Image></a>
-                <a><Image src={facebook} height={16} width={16}></Image></a>
+              Follow us on:
+              <a>
+                <Image src={twitter} height={16} width={16}></Image>
+              </a>
+              <a>
+                <Image src={instagram} height={16} width={16}></Image>
+              </a>
+              <a>
+                <Image src={facebook} height={16} width={16}></Image>
+              </a>
             </div>
-        </div>
-        <hr className={styles.footerLine}/>
-        <div className={styles.footerSection3}>
-            <Link href="/"><a>Terms of use</a></Link>
-            <Link href="/"><a>Privacy Policy</a></Link>
-            <Link href="/"><a>About us</a></Link>
-            <Link href="/"><a>NewsLetter</a></Link>
-            <Link href="/"><a>License Footage</a></Link>
-            <Link href="/"><a>Accesibility</a></Link>
-        </div>
-        <div className={styles.cpright}>© 2021  News Network.All Rights Reserved.</div>
+          </div>
+          <hr className={styles.footerLine} />
+          <div className={styles.footerSection3}>
+            <Link href="/">
+              <a>Terms of use</a>
+            </Link>
+            <Link href="/">
+              <a>Privacy Policy</a>
+            </Link>
+            <Link href="/">
+              <a>About us</a>
+            </Link>
+            <Link href="/">
+              <a>NewsLetter</a>
+            </Link>
+            <Link href="/">
+              <a>License Footage</a>
+            </Link>
+            <Link href="/">
+              <a>Accesibility</a>
+            </Link>
+          </div>
+          <div className={styles.cpright}>
+            © 2021 News Network.All Rights Reserved.
+          </div>
         </footer>
-       </body>
-        </>
-    )
+      </body>
+    </>
+  );
 }
--- pages/products.js
+++ pages/products.js
@@ -1,12 +1,18 @@
 import Image from 'next/image';
 import Layout from '../components/layout';
+import { useRouter } from 'next/router';
 import styles from '../styles/products.module.css';
 import ProductCart from '../components/ProductCart';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import star from '../public/logos/star.svg';
+import verified from '../public/logos/verified.svg';
 
 export default function aboutUs() {
+  const router = useRouter();
+
   return (
     <Layout page={3}>
       <section className={styles.section1}>
@@ -80,17 +86,27 @@ export default function aboutUs() {
 
       <Container fluid>
         <Row style={{ backgroundColor: '#AA131D', minWidth: '300px' }}>
-          <Col className="d-flex flex-column align-items-center">
-            <div>
+          <Col className="d-flex flex-column align-items-center justify-content-center">
+            <div
+              className="mb-md-5 mb-xs-1 mb-sm-2"
+              style={{ maxWidth: '424px' }}
+            >
               <p className="fs-5 fs-sm-3 fs-md-1 text-center">
                 Grab a cup and seize the day
               </p>
             </div>
             <div>
-              <p className="fs-6 fs-sm-4 fs-md-3">High collagen</p>
-              <p className="fs-6 fs-sm-4 fs-md-3">High protein</p>
-              <p className="fs-6 fs-sm-4 fs-md-3">Big on flavor</p>
+              <p className="fs-6 fs-sm-5 fs-md-3">High collagen</p>
+              <p className="fs-6 fs-sm-5 fs-md-3">High protein</p>
+              <p className="fs-6 fs-sm-5 fs-md-3">Big on flavor</p>
             </div>
+            <Button
+              className="mb-1"
+              variant="dark"
+              onClick={() => router.push('/contact-us')}
+            >
+              Contact us
+            </Button>
           </Col>
 
           <Col className="d-flex align-items-center justify-content-center">
@@ -107,21 +123,131 @@ export default function aboutUs() {
         </Row>
 
         <Row style={{ backgroundColor: '#D75B98' }}>
-          <Col xs>
-            <div style={{ maxWidth: '657', maxHeight: '639' }}>
+          <Col className="d-flex align-items-center justify-content-center">
+            <div
+              style={{
+                minWidth: '190px',
+                maxWidth: '657px',
+                maxHeight: '639px',
+              }}
+            >
               <Image src={require('../public/products/female.png')} />
             </div>
           </Col>
+          <Col className="d-flex flex-column align-items-center justify-content-center">
+            <div
+              className="mb-md-5 mb-xs-1 mb-sm-2"
+              style={{ maxWidth: '424px' }}
+            >
+              <p className="fs-5 fs-sm-3 fs-md-1 text-center">
+                Grab a cup and seize the day
+              </p>
+            </div>
+            <div>
+              <p className="fs-6 fs-sm-5 fs-md-3">High collagen</p>
+              <p className="fs-6 fs-sm-5 fs-md-3">High protein</p>
+              <p className="fs-6 fs-sm-5 fs-md-3">Big on flavor</p>
+            </div>
 
-          <Col> hello</Col>
+            <Button
+              className="mb-1"
+              variant="dark"
+              onClick={() => router.push('/contact-us')}
+            >
+              Contact us
+            </Button>
+          </Col>
         </Row>
 
         <Row className={'text-dark'} style={{ backgroundColor: '#F6EEC7' }}>
-          <Col> hello</Col>
+          <Col className="d-flex flex-column align-items-center justify-content-center">
+            <div
+              className="mb-md-5 mb-xs-1 mb-sm-2"
+              style={{ maxWidth: '424px' }}
+            >
+              <p className="fs-5 fs-sm-3 fs-md-1 text-center">
+                Grab a cup and seize the day
+              </p>
+            </div>
+            <div>
+              <p className="fs-6 fs-sm-5 fs-md-3">High collagen</p>
+              <p className="fs-6 fs-sm-5 fs-md-3">High protein</p>
+              <p className="fs-6 fs-sm-5 fs-md-3">Big on flavor</p>
+            </div>
+            <Button
+              className="mb-1"
+              variant="dark"
+              onClick={() => router.push('/contact-us')}
+            >
+              Contact us
+            </Button>
+          </Col>
 
-          <Col xs>
-            <div style={{ maxWidth: '657', maxHeight: '639' }}>
+          <Col className="d-flex align-items-center justify-content-center">
+            <div
+              style={{
+                minWidth: '190px',
+                maxWidth: '657px',
+                maxHeight: '639px',
+              }}
+            >
               <Image src={require('../public/products/female.png')} />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+
+      <Container fluid className="pt-5" style={{ backgroundColor: '#000' }}>
+        <Row className="mt-5 d-flex justify-content-around">
+          <Col
+            xs
+            sm={4}
+            className="d-flex flex-column align-items-center mb-5"
+            style={{ maxWidth: '402px', minWidth: '292px' }}
+          >
+            <div>
+              <Image src={star} width={238} height={37} />
+            </div>
+            <p className="text-center">
+              “I love it and use it to cook with. Great for soups, stirs fries.”
+            </p>
+            <p>-Binod karki</p>
+            <div>
+              <Image src={verified} width={238} height={37} />
+            </div>
+          </Col>
+          <Col
+            xs
+            sm={4}
+            className="d-flex flex-column align-items-center mb-5"
+            style={{ maxWidth: '402px', minWidth: '292px' }}
+          >
+            <div>
+              <Image src={star} width={238} height={37} />
+            </div>
+            <p className="text-center">
+              “I love it and use it to cook with. Great for soups, stirs fries.”
+            </p>
+            <p>-Binod karki</p>
+            <div>
+              <Image src={verified} width={238} height={37} />
+            </div>
+          </Col>
+          <Col
+            xs
+            sm={4}
+            className="d-flex flex-column align-items-center mb-5"
+            style={{ maxWidth: '402px', minWidth: '292px' }}
+          >
+            <div>
+              <Image src={star} width={238} height={37} />
+            </div>
+            <p className="text-center">
+              “I love it and use it to cook with. Great for soups, stirs fries.”
+            </p>
+            <p>-Binod karki</p>
+            <div>
+              <Image src={verified} width={238} height={37} />
             </div>
           </Col>
         </Row>
