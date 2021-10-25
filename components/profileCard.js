58e3425667d5915714de9aa267500324e1794fa9
SameerCrestha <ddav.crestha@gmail.com>
2021-10-25 18:28:00 +0545

    minor modifications

--- components/profileCard.js
+++ components/profileCard.js
@@ -3,16 +3,16 @@ import styles from "./profileCard.module.css";
 export default function ProfileCard({name,post,detail,pic}){
     return(
             <div className={styles.container}>
-               <div>
+               <div className={styles.pic}>
+                  <Image src={pic} layout="responsive" alt="profile pic"/>
+               </div>
+               <div className={styles.info}>
                <span>{name}</span><br/>
                <small>{post}</small>
                <p>
                 {detail}
                </p>
-               </div>
-               <div className={styles.pic}>
-                  <Image src={pic} layout="responsive" alt="profile pic"/>
-               </div>
+               </div>  
             </div>
     );
 }
