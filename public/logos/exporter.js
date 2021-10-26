a32c6230843adffb3538ef35a77311ba898e8905
SameerCrestha <ddav.crestha@gmail.com>
2021-10-26 19:28:40 +0545

    minor change

--- public/logos/exporter.js
+++ public/logos/exporter.js
@@ -5,7 +5,5 @@ import mail from "./mail.svg";
 import phone from "./phone.svg";
 import twitter from "./twitter.svg";
 import linkedin from "./linkedin.svg";
-import checkSquare from "./check-square.svg";
-import alarm from "./alarm.svg";
-const icons={fb,insta,location,mail,phone,twitter,linkedin,checkSquare,alarm};
+const icons={fb,insta,location,mail,phone,twitter,linkedin};
 export default icons;
