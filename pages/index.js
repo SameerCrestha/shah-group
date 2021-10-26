c4de6895f5e1800bd37bb672709fcad83066f92e
SameerCrestha <ddav.crestha@gmail.com>
2021-10-26 19:48:35 +0545

    fixed bug in module import

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import styles from '../styles/Home.module.css'
 import images from '../public/index/sec1card2.png';
 export default function Home() {
